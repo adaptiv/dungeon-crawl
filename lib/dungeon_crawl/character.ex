b1c8ac6aa2b605d0d530733133e4139ec7124d1c
Joakim Manding Holm <joakim.manding.holm@adaptiv.se>
2018-04-06 22:37:21 +0200

    Print welcome message and select hero

---
+++ lib/dungeon_crawl/character.ex
@@ -0,0 +1,8 @@
+defmodule DungeonCrawl.Character do
+  defstruct name: nil,
+            description: nil,
+            hit_points: 0,
+            max_hit_points: 0,
+            attact_description: nil,
+            damage_range: nil
+end
---
+++ lib/dungeon_crawl/cli/main.ex
@@ -0,0 +1,19 @@
+defmodule DungeonCrawl.CLI.Main do
+  alias Mix.Shell.IO, as: Shell
+
+  def start_game() do
+    welcome_message()
+    Shell.prompt("Press enter to continue")
+    hero_choice()
+  end
+
+  defp welcome_message() do
+    Shell.info("=== DungeonCrawl ===")
+    Shell.info("You awake in a dungeon full of monsters.")
+    Shell.info("You need to survive and find the exit.")
+  end
+
+  defp hero_choice do
+    DungeonCrawl.CLI.HeroChoice.start()
+  end
+end
