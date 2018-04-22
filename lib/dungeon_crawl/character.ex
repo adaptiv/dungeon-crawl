964203756c78fb5597204ca8ad951db4299d127b
Joakim Manding Holm <joakim.manding.holm@adaptiv.se>
2018-04-22 13:56:48 +0200

    Add type specification for run/2

--- lib/dungeon_crawl/character.ex
+++ lib/dungeon_crawl/character.ex
@@ -3,8 +3,17 @@ defmodule DungeonCrawl.Character do
             description: nil,
             hit_points: 0,
             max_hit_points: 0,
-            attact_description: nil,
+            attack_description: nil,
             damage_range: nil
+
+  @type t :: %DungeonCrawl.Character{
+          name: String.t(),
+          description: String.t(),
+          hit_points: non_neg_integer,
+          max_hit_points: non_neg_integer,
+          attack_description: String.t(),
+          damage_range: Range.t()
+        }
 
   defimpl String.Chars do
     def to_string(character), do: character.name
--- lib/dungeon_crawl/heroes.ex
+++ lib/dungeon_crawl/heroes.ex
@@ -8,24 +8,24 @@ defmodule DungeonCrawl.Heroes do
         description: "Knight has a strong defense and consistent damage.",
         hit_points: 18,
         max_hit_points: 18,
-        damage_range: 4..5,
-        attact_description: "a sword"
+        attack_description: "a sword",
+        damage_range: 4..5
       },
       %Character{
         name: "Wizard",
         description: "Wizard has a strong attact but low health.",
         hit_points: 8,
         max_hit_points: 8,
-        damage_range: 6..10,
-        attact_description: "a fireball"
+        attack_description: "a fireball",
+        damage_range: 6..10
       },
       %Character{
         name: "Rogue",
         description: "Rogue has high variability of attack damage.",
         hit_points: 12,
         max_hit_points: 12,
-        damage_range: 1..12,
-        attact_description: "a dagger"
+        attack_description: "a dagger",
+        damage_range: 1..12
       }
     ]
 end
