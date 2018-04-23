b3b67a626c80fcfda385954634aaf5f611fb6683
Joakim Manding Holm <joakim.manding.holm@adaptiv.se>
2018-04-23 10:39:38 +0200

    Add enemies and fights.

--- lib/dungeon_crawl/heroes.ex
+++ lib/dungeon_crawl/heroes.ex
@@ -8,24 +8,24 @@ defmodule DungeonCrawl.Heroes do
         description: "Knight has a strong defense and consistent damage.",
         hit_points: 18,
         max_hit_points: 18,
-        attack_description: "a sword",
-        damage_range: 4..5
+        damage_range: 4..5,
+        attack_description: "a sword"
       },
       %Character{
         name: "Wizard",
         description: "Wizard has a strong attact but low health.",
         hit_points: 8,
         max_hit_points: 8,
-        attack_description: "a fireball",
-        damage_range: 6..10
+        damage_range: 6..10,
+        attack_description: "a fireball"
       },
       %Character{
         name: "Rogue",
         description: "Rogue has high variability of attack damage.",
         hit_points: 12,
         max_hit_points: 12,
-        attack_description: "a dagger",
-        damage_range: 1..12
+        damage_range: 1..12,
+        attack_description: "a dagger"
       }
     ]
 end
