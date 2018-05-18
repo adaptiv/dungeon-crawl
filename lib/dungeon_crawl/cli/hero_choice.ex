4b0f9424d0355bc4c475eb6d5498e77a1fee119a
Joakim Manding Holm <joakim.manding.holm@adaptiv.se>
2018-05-18 23:30:46 +0200

    print error message also for invalid hero choice

--- lib/dungeon_crawl/cli/hero_choice.ex
+++ lib/dungeon_crawl/cli/hero_choice.ex
@@ -5,16 +5,17 @@ defmodule DungeonCrawl.CLI.HeroChoice do
   def start do
     Shell.cmd("clear")
     Shell.info("Start by choosing your hero:")
+    select_hero(DungeonCrawl.Heroes.all())
+  end
 
-    heroes = DungeonCrawl.Heroes.all()
-
+  defp select_hero(heroes) do
     heroes
     |> display_options
     |> generate_question
     |> Shell.prompt()
     |> parse_numerical_answer
     |> find_hero_by_index(heroes)
-    |> confirm_hero
+    |> confirm_hero(heroes)
   end
 
   defp find_hero_by_index(index, heroes)
@@ -25,11 +26,18 @@ defmodule DungeonCrawl.CLI.HeroChoice do
     Enum.at(heroes, index)
   end
 
-  defp confirm_hero(:error), do: start()
+  defp confirm_hero(:error, heroes), do: handle_invalid_choice(heroes)
 
-  defp confirm_hero(chosen_hero) do
+  defp confirm_hero(chosen_hero, _) do
     Shell.info("")
     Shell.info(chosen_hero.description)
     if Shell.yes?("Confirm?"), do: chosen_hero, else: start()
+  end
+
+  defp handle_invalid_choice(heroes) do
+    Shell.info("")
+    Shell.info("Please select one of the options...")
+    Shell.info("")
+    select_hero(heroes)
   end
 end
